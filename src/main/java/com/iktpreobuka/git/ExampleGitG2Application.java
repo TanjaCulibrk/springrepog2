79a03d3f129ebb4a01bce12c3e8a283bc95b1258
Tanja Culibrk <tanja.culibrk.brains22@gmail.com>
2023-02-14 20:43:28 +0100

    moj prvi commit

---
+++ src/main/java/com/iktpreobuka/git/ExampleGitG2Application.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitG2Application.class, args);
+	}
+
+}
